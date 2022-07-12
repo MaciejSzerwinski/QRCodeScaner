00621566322cb42a408962bfd1ca44afffa2cd1f
Maciej Szerwinski <maciej.szerwinski@gmail.com>
2022-07-12 13:00:20 +0200

    detect auth

--- src/qrCodeScanner.js
+++ src/qrCodeScanner.js
@@ -2,7 +2,7 @@ const video = document.createElement("video");
 const canvasElement = document.getElementById("qr-canvas");
 const canvas = canvasElement.getContext("2d");
 
-const qrResult = document.getElementById("qr-result");
+const qrResult = document.getElementById("qr-login");
 const outputData = document.getElementById("outputData");
 const btnScanQR = document.getElementById("btn-scan-qr");
 
