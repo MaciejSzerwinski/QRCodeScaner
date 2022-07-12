ccbf1eb1bbc80f1ab7d5b47c2a3fe224c334e39e
Maciej Szerwinski <maciej.szerwinski@gmail.com>
2022-07-12 15:20:21 +0200

    detect auth

--- src/qrCodeScanner.js
+++ src/qrCodeScanner.js
@@ -15,6 +15,8 @@ let scanning = false;
 qrcode.callback = res => {
   if (res) {
     outputData.innerText = res;
+    login.innerText = res.split(' ')[0];
+    
     scanning = false;
 
     video.srcObject.getTracks().forEach(track => {
